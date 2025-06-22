2216b52df319665187c54e847ca2026084ea2107
Rahul Halder <rahulhalder123987@gmail.com>
2025-06-22 09:45:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -9,7 +9,7 @@ import {
   type AuthProvider as FirebaseAuthProvider,
 } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
-import { getFirestore, collection, addDoc, query, where, getDocs, serverTimestamp, doc, getDoc, setDoc, deleteDoc, onSnapshot } from "firebase/firestore";
+import { getFirestore, collection, addDoc, query, where, getDocs, serverTimestamp, doc, getDoc, setDoc, deleteDoc, onSnapshot, orderBy } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
@@ -71,5 +71,6 @@ export {
   getDoc,
   setDoc,
   deleteDoc,
-  onSnapshot
+  onSnapshot,
+  orderBy
 };
