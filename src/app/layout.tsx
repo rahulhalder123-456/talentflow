62cd597a2e195abd4efc634d3a617dbe99d21840
Rahul Halder <rahulhalder123987@gmail.com>
2025-08-02 05:36:14 +0000

    use Lenis for smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/hooks/use-auth';
+import { LenisProvider } from '@/components/common/LenisProvider';
 
 // This metadata will be applied to the page but can be overridden
 // export const metadata: Metadata = {
@@ -25,10 +26,12 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&family=Sora:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AuthProvider>
-            {children}
-            <Toaster />
-        </AuthProvider>
+        <LenisProvider>
+          <AuthProvider>
+              {children}
+              <Toaster />
+          </AuthProvider>
+        </LenisProvider>
       </body>
     </html>
   );
---
+++ src/components/common/LenisProvider.tsx
@@ -0,0 +1,28 @@
+
+"use client";
+
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+
+export function LenisProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+        lerp: 0.1,
+        duration: 1.2,
+        smoothTouch: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+    
+    return () => {
+        lenis.destroy();
+    }
+  }, []);
+
+  return <>{children}</>;
+}
